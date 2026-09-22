cdb46373f15561404210d69b4f4aed125eac564e
Jaqueline <jaquelineazanin@outlook.com>
2026-09-21 21:34:56 -0300

    feat: implementei API de usuários com persistência

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async findAll() {
+    const result = await this.databaseService.query(
+      `
+        SELECT id, name, email, created_at
+        FROM users
+        ORDER BY created_at DESC
+      `,
+    );
+
+    return result.rows;
+  }
+  async create(createUserDto: CreateUserDto) {
+  const result = await this.databaseService.query(
+    `
+      INSERT INTO users (name, email)
+      VALUES ($1, $2)
+      RETURNING id, name, email, created_at
+    `,
+    [createUserDto.name, createUserDto.email],
+  );
+
+  return result.rows[0];
+}
+}
